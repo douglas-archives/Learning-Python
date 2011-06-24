8fea7956d48d8b4ea9c618fc7fd42f0e39efa31c
Douglas Miranda <douglasmirandasilva@gmail.com>
2011-06-24 00:06:38 -0400

    Eliminando código duplicado e adicionando comentários

--- listas.py
+++ listas.py
@@ -3,12 +3,10 @@ lista_string = ['bbb', 'yyy', 'mmm', 'aaa', 'www', 'ddd']
 lista_string.sort()
 print lista_string
 
-#ao passar numeros a lista sera ordenada do menor para o maior
 lista_numeros = [521, 123, 52, 5, 546546536]
-lista_numeros.sort()
 print lista_numeros
 
-lista_numeros = [521, 123, 52, 5, 546546536]
+#ao passar numeros a lista sera ordenada do menor para o maior
 lista_numeros.sort()
 print lista_numeros
 
@@ -17,6 +15,7 @@ lista = ['aaaa', 'z', 'bbbbbbbbbb', 'nnn']
 def porTamanho(item):
     return len(item)
 
+# Observe que estou passando como parametro uma funcao :) parecido com javascript
 print sorted(lista, key=porTamanho)
 
 #se eu quiser embaralhar a lista acima...
